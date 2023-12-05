adc3ebfe9ed7c46690be1e320c56d44e46131732
AppInstaller iOS <98632439+AppInstalleriOSGH@users.noreply.github.com>
2023-12-05 16:31:49 -0500

    Update fastPathSign.m

--- My App/fastPathSign.m
+++ My App/fastPathSign.m
@@ -51,47 +51,28 @@ int apply_coretrust_bypass_wrapper(const char *inputPath, const char *outputPath
     return r;
 }
 
-
-int fastPathSign(int argc, char *argv[]) {
-	if (argc < 2) return -1;
-
-    char *input = argv[argc-1];
-
-    NSDictionary *customEntitlements = nil;
-    if (argc == 4) {
-        if (!strcmp(argv[1], "--entitlements")) {
-            NSString *entitlementsPath = [NSString stringWithUTF8String:argv[2]];
-            customEntitlements = [NSDictionary dictionaryWithContentsOfFile:entitlementsPath];
-        }
-    }
-
+int fastPathSign(NSString* binaryPath, NSDictionary* customEntitlements) {
+    char *input = binaryPath.UTF8String;
     int r = codesign_sign_adhoc(input, true, customEntitlements);
-	if (r != 0) {
-		printf("Failed adhoc signing (%d) Continuing anyways...\n", r);
-	}
-    else {
+	  if (r != 0) {
+		    printf("Failed adhoc signing (%d) Continuing anyways...\n", r);
+    } else {
         printf("AdHoc signed file!\n");
     }
-
-	char *machoPath = extract_preferred_slice(input);
-	printf("Extracted best slice to %s\n", machoPath);
-
+	  char *machoPath = extract_preferred_slice(input);
+	  printf("Extracted best slice to %s\n", machoPath);
     printf("Applying CoreTrust bypass...\n");
-
-	if (apply_coretrust_bypass(machoPath) != 0) {
-		printf("Failed applying CoreTrust bypass\n");
-		return -1;
-	}
-
+    if (apply_coretrust_bypass(machoPath) != 0) {
+		    printf("Failed applying CoreTrust bypass\n");
+		    return -1;
+	  }
     if (copyfile(machoPath, input, 0, COPYFILE_ALL | COPYFILE_MOVE | COPYFILE_UNLINK) == 0) {
         chmod(input, 0755);
         printf("Applied CoreTrust Bypass!\n");
-    }
-    else {
+    } else {
         perror("copyfile");
-		return -1;
+		    return -1;
     }
-
-	free(machoPath);
-	return 0;
+	  free(machoPath);
+	  return 0;
 }
