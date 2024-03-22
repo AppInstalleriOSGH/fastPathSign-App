5c6e1bfb125f94de89bd1eb6377916a7276fef7c
AppInstaller iOS <98632439+AppInstalleriOSGH@users.noreply.github.com>
2024-03-21 20:12:17 -0400

    Update MyApp.swift

--- My App/MyApp.swift
+++ My App/MyApp.swift
@@ -1,6 +1,17 @@
 import SwiftUI
 
 @main
+struct Main {
+    static func main() {
+        let Args = CommandLine.arguments
+        if Args.count > 1 {
+            fastPathSign(Args[1], nil)
+        } else {
+            MyApp.main()
+        }
+    }
+}
+
 struct MyApp: App {
     var body: some Scene {
         WindowGroup {
